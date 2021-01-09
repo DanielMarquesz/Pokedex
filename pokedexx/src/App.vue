98cba8f2dbe7c1144d427c095eedde6ee674b54b
Daniel Marques <daniel.marques142@gmail.com>
2021-01-09 01:43:28 -0300

    feat: create component Pokemom

--- pokedexx/src/App.vue
+++ pokedexx/src/App.vue
@@ -1,14 +1,38 @@
 <template>
   <div id="app">
-    
+    <div v-for="(poke, index) in pokemons" :key="index">
+      <!-- <h1>{{pokemons[index].name}}</h1>
+      <h3>{{pokemons[index].url}}</h3><br> -->
+
+      <Pokemom :name="poke.name" :url="poke.url" :num="index+1" />
+    </div>
   </div>
 </template>
 
 <script>
-//import axios from 'axios';
+import axios from 'axios';
+import Pokemom from './components/Pokemon';
 
 export default {
-  name: 'App'
+  name: 'App',
+
+  data(){
+      return {
+        pokemons: []
+    }
+  },
+
+  created: function() {
+    axios.get("https://pokeapi.co/api/v2/pokemon?limit=100&offset=200").then((response)=>{
+      
+      this.pokemons = response.data.results;
+      console.log(this.pokemons)
+    })
+  },
+
+  components: {
+    Pokemom
+  }
 }
 </script>
 
---
+++ pokedexx/src/components/Pokemon.vue
@@ -0,0 +1,23 @@
+
+<template>
+    <div>
+        <h1>{{name}}</h1>
+        <small>{{url}}</small>
+        <h1>{{num}}</h1>
+    </div>    
+</template>
+
+<script>
+export default {
+    props: {
+        num: Number,
+        name: String,
+        url: String
+    }
+}
+</script>
+
+
+<style scoped>
+
+</style>
